18e6de37b42c199da0cb77bdf3b0cfcaf996be6d
sam89f <sam89f@gmail.com>
2021-12-16 22:53:28 -0600

    changed title at the top of main file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Hebrew Dictionary mobile aplication python file
+#Hebrew Dictionary mobile aplication python file (Tablet Version)
 #==============================================================================
 
 #This file displays the UI, and the main fuction buttons and input text field, while the .kv file
