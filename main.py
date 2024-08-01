6e7b638ee59dbfedf0319576fbd01805f389f8d9
sam89f <sam89f@gmail.com>
2024-07-31 23:22:43 -0500

    readded words

--- main.py
+++ main.py
@@ -4110,7 +4110,7 @@ class HebrewDictionary(App):
                         else:
                             hifilW.setVerbform(rareVerbforms[word2.getVerbform()])
                             
-                        return self.smPrefix(look, hifilW, True, Check)
+                        return self.ssmPrefix(look, hifilW, True, Check)
                 
                 if(word2.nextToFirst() == 'ו'):
                     if(word2.first() == 'ה') and (not(word2.getRootFirst2() == word2.first2())):
@@ -4478,7 +4478,7 @@ class HebrewDictionary(App):
                     if(word.first() == 'ל'):
                         self.infinitive(look, hufalW, Check)
                     if(self.prefixRuls(word, word.first(), True) == True):
-                        return self.Prefix(look, hufalW, True, Check)
+                        return self.prefix(look, hufalW, True, Check)
                     else:
                         return hufalW
 
@@ -4600,12 +4600,12 @@ class HebrewDictionary(App):
                     
                     if(word.first() == 'ל'):
                         self.infinitive(look, hollow, False)
-                    self.Prefix(look, hollow, True, Check)
+                    self.prefix(look, hollow, True, Check)
                     
                 if(word.first() == 'ל'):
                     self.infinitive(look, hitpaelW, Check)
                 if(self.prefixRuls(word, word.first(), True) == True):
-                    return self.Prefix(look, hitpaelW, True, Check)
+                    return self.prefix(look, hitpaelW, True, Check)
                 else:
                     return hitpaelW
                 
